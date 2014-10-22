12d902263a66839ad3ddd4fc83fd885ccb85f622
Josh Lapham <josh@joshlapham.com>
2014-10-22 11:48:45 +1100

    Initial Commit

---
+++ Jore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jore
+//
+//  Created by jl on 22/10/2014.
+//  Copyright (c) 2014 Josh Lapham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jore
+//
+//  Created by jl on 22/10/2014.
+//  Copyright (c) 2014 Josh Lapham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
