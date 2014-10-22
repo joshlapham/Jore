1cc6a47214cfbf374b48d9418082e0fe7ed06e08
Josh Lapham <josh@joshlapham.com>
2014-10-22 17:11:13 +1100

    Implemented JTrack data model.

--- Jore/Models/JTrack.h
+++ Jore/Models/JTrack.h
@@ -8,6 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
-@interface JTrack : NSObject
+@interface JTrack : NSObject <NSCoding>
+
+@property (nonatomic, strong) NSString *trackName;
+@property (nonatomic, strong) NSString *trackId;
+@property (nonatomic, strong) NSString *trackDuration;
+@property (nonatomic, strong) NSString *trackNumber;
+@property (nonatomic, strong) NSString *trackPreviewUrl;
+@property (nonatomic, strong) NSString *trackBelongsToAlbumId;
+
+- (id)initWithName:(NSString *)trackNameValue
+             andId:(NSString *)trackIdValue
+       andDuration:(NSString *)trackDurationValue
+         andNumber:(NSString *)trackNumberValue
+     andPreviewUrl:(NSString *)trackPreviewUrlValue
+        andAlbumId:(NSString *)trackAlbumIdValue;
 
 @end
--- Jore/Models/JTrack.m
+++ Jore/Models/JTrack.m
@@ -10,4 +10,52 @@
 
 @implementation JTrack
 
+@synthesize trackName, trackId, trackDuration, trackNumber, trackPreviewUrl, trackBelongsToAlbumId;
+
+#pragma mark - Init method
+
+- (id)initWithName:(NSString *)trackNameValue
+             andId:(NSString *)trackIdValue
+       andDuration:(NSString *)trackDurationValue
+         andNumber:(NSString *)trackNumberValue
+     andPreviewUrl:(NSString *)trackPreviewUrlValue
+        andAlbumId:(NSString *)trackAlbumIdValue {
+    self = [super init];
+    if (self) {
+        trackName = trackNameValue;
+        trackId = trackIdValue;
+        trackDuration = trackDurationValue;
+        trackNumber = trackNumberValue;
+        trackPreviewUrl = trackPreviewUrlValue;
+        trackBelongsToAlbumId = trackAlbumIdValue;
+    }
+    return self;
+}
+
+#pragma mark - NSCoding delegate methods
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super init];
+    if (self) {
+        trackName = [aDecoder decodeObjectForKey:@"trackName"];
+        trackId = [aDecoder decodeObjectForKey:@"trackId"];
+        trackDuration = [aDecoder decodeObjectForKey:@"trackDuration"];
+        trackNumber = [aDecoder decodeObjectForKey:@"trackNumber"];
+        trackPreviewUrl = [aDecoder decodeObjectForKey:@"trackPreviewUrl"];
+        trackBelongsToAlbumId = [aDecoder decodeObjectForKey:@"trackBelongsToAlbumId"];
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    [aCoder encodeObject:trackName forKey:@"albumName"];
+    [aCoder encodeObject:trackId forKey:@"trackId"];
+    [aCoder encodeObject:trackDuration forKey:@"trackDuration"];
+    [aCoder encodeObject:trackNumber forKey:@"trackNumber"];
+    [aCoder encodeObject:trackPreviewUrl forKey:@"trackPreviewUrl"];
+    [aCoder encodeObject:trackBelongsToAlbumId forKey:@"trackBelongsToAlbumId"];
+}
+
 @end
